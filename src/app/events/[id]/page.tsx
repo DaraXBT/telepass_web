4b1212a51c19015414119330d4e93fc827d1bc99
Dara Veasna <112950879+DaraXBT@users.noreply.github.com>
2025-06-13 14:57:39 +0700

    refactor: update event detail and list pages for improved structure and consistency

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -2,20 +2,22 @@
 
 import {EventPage} from "@/components/events/event-page";
 import MenuNav from "@/components/nav-menu";
-import {Separator} from "@radix-ui/react-separator";
+import {Separator} from "@/components/ui/separator";
 import {useLanguage} from "@/components/providers/LanguageProvider";
 
 export default function EventDetailPage() {
   const {t} = useLanguage();
 
   return (
-    <div className="container mx-auto p-6">
+    <div>
       <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <MenuNav props={t("Event List")} />
       </header>
       <Separator />
-      <h1 className="text-3xl font-semibold mb-6">{t("Event Details")}</h1>
-      <EventPage />
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-semibold mb-6">{t("Event Details")}</h1>
+        <EventPage />
+      </div>
     </div>
   );
 }
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import MenuNav from "@/components/nav-menu";
-import {Suspense} from "react";
 import {EventList} from "@/components/events/event-list";
 import {Separator} from "@/components/ui/separator";
 import {useLanguage} from "@/components/providers/LanguageProvider";
@@ -13,7 +12,7 @@ export default function EventListPage() {
     <div>
       <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <MenuNav props={t("Event List")} />
-      </header>{" "}
+      </header>
       <Separator />
       <div className="p-4">
         <div className="flex items-center justify-between mb-8">
